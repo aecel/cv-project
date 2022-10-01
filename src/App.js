cc236d44d2ad09bd7d632520c7ac5d50c1c68df6
aecel <aecelacuna@gmail.com>
2022-10-02 00:18:51 +0800

    Add working pdf export

--- src/App.js
+++ src/App.js
@@ -52,32 +52,112 @@ const App = () => {
 
   useEffect(() => {
     const myPdf = new jsPDF()
-    myPdf.getFontList()
-    // myPdf.setFont("Lato-Regular", "normal")
-    // Filled dark gray rectangle
-    myPdf.setDrawColor(0)
-    myPdf.setFillColor(29, 29, 29)
-    myPdf.rect(0, 0, 211, 30, "F")
 
     // Filled light gray rectangle
     myPdf.setDrawColor(0)
-    myPdf.setFillColor(197, 197, 197)
+    myPdf.setFillColor(225, 225, 225)
     myPdf.rect(0, 30, 211, 267, "F")
 
-    myPdf.setFillColor("#1d1d1d")
-    myPdf.setFont("Lato-Bold", "bold")
-    myPdf.setTextColor("#c5c5c5")
-    const text = JSON.stringify(education)
-    const splitText = myPdf.splitTextToSize(text, 180)
-    myPdf.text(splitText, 10, 10)
+    // Filled dark gray rectangle
+    myPdf.setDrawColor(0)
+    myPdf.setFillColor(29, 29, 29)
+    myPdf.rect(0, 0, 211, 50, "F")
 
-    var d = myPdf.getTextDimensions(splitText)
-    console.log(d)
-    // myPdf.text("I am a baby deer -acey", 10, 24)
+    // Full Name
+    myPdf.setFont("Lato-Bold", "bold")
+    myPdf.setFontSize(26)
+    myPdf.setTextColor("#ffffff")
+    myPdf.text(info.fullName, 10, 18)
+
+    // Current Role
+    myPdf.setFontSize(12)
+    myPdf.setTextColor("#c5c5c5")
+    myPdf.setFont("Lato-Regular", "normal")
+    myPdf.text(info.role, 10, 25)
+
+    // Email and Phone Number
+    myPdf.setFontSize(10)
+    myPdf.setTextColor("#c5c5c5")
+    myPdf.text(info.email, 10, 34)
+    myPdf.text(info.phoneNumber, 10, 41)
+
+    // Start of light gray area is (10,60)
+    myPdf.setTextColor("#1d1d1d")
+    myPdf.setFont("Lato-Bold", "bold")
+    myPdf.setFontSize(14)
+
+    // Professional Experience
+    let nextY = 60
+    myPdf.text("Professional Experience", 10, nextY)
+    myPdf.setDrawColor(29, 29, 29)
+    myPdf.line(10, nextY + 3, 190, nextY + 3, "S")
+    nextY += 10
+
+    // Preparing for each job
+    myPdf.setFont("Lato-Regular", "normal")
+    myPdf.setFontSize(10)
+
+    for (const job of jobs) {
+      nextY += 2
+      myPdf.setFont("Lato-Bold", "bold")
+      myPdf.text(job.company, 10, nextY)
+      myPdf.text(job.role, 10, nextY + 6)
+      myPdf.setFont("Lato-Regular", "normal")
+      const splitText = myPdf.splitTextToSize(job.description, 180)
+      myPdf.text(splitText, 10, nextY + 12)
+      const dimension = myPdf.getTextDimensions(splitText)
+      nextY = nextY + dimension.h + 20
+    }
+
+    // Skills
+    nextY += 2
+    myPdf.setTextColor("#1d1d1d")
+    myPdf.setFont("Lato-Bold", "bold")
+    myPdf.setFontSize(14)
+    myPdf.text("Skills", 10, nextY)
+    myPdf.setDrawColor(29, 29, 29)
+    myPdf.line(10, nextY + 3, 190, nextY + 3, "S")
+    nextY += 10
+
+    // Preparing for each skill
+    myPdf.setFont("Lato-Regular", "normal")
+    myPdf.setFontSize(10)
+
+    for (const skill of skills) {
+      nextY += 2
+      myPdf.setFont("Lato-Bold", "bold")
+      myPdf.text(skill.name, 10, nextY)
+      myPdf.setFont("Lato-Regular", "normal")
+      const splitText = myPdf.splitTextToSize(skill.description, 180)
+      myPdf.text(splitText, 10, nextY + 6)
+      const dimension = myPdf.getTextDimensions(splitText)
+      nextY = nextY + dimension.h + 14
+    }
+
+    // Education
+    nextY += 2
+    myPdf.setTextColor("#1d1d1d")
+    myPdf.setFont("Lato-Bold", "bold")
+    myPdf.setFontSize(14)
+    myPdf.text("Education", 10, nextY)
+    myPdf.setDrawColor(29, 29, 29)
+    myPdf.line(10, nextY + 3, 190, nextY + 3, "S")
+    nextY += 10
+
+    // Preparing for education
+    myPdf.setFont("Lato-Regular", "normal")
+    myPdf.setFontSize(10)
+
+    myPdf.setFont("Lato-Bold", "bold")
+    nextY += 2
+    myPdf.text(education.universityName, 10, nextY)
+    myPdf.setFont("Lato-Regular", "normal")
+    myPdf.text(education.degree, 10, nextY + 6)
+    nextY = nextY + 16
 
     setPdf(myPdf.output("datauristring", "mycv.pdf"))
     // myPdf.save("cv.pdf")
-  }, [education])
+  }, [info, jobs, skills, education])
 
   const changeInfoText = (infoParameter, e) => {
     const nextInfo = { ...info }
